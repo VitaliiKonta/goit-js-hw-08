44850beb3bb5f85c29445182721dc204fa9c69f9
VitaliiKonta <120408874+VitaliiKonta@users.noreply.github.com>
2023-05-05 20:04:26 +0300

    hw-08  gallery-01 is ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryMarkupEl = document.querySelector('ul.gallery');
+const addMarkupItemsEl = addMarkupItems(galleryItems);
+galleryMarkupEl.insertAdjacentHTML('beforeend', addMarkupItemsEl);
+
+function addMarkupItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+         <a class = "gallery__item" href = "${original}">
+         <img class = "gallery__image" src="${preview}" alt="${description}">
+         </a>
+        `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
