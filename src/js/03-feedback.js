a1643c9a4ff55919816ed5dd4e653c6057b16f70
VitaliiKonta <120408874+VitaliiKonta@users.noreply.github.com>
2023-05-10 22:29:50 +0300

    hw-8 is ready

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
+import throttle from 'lodash.throttle';
 
+const STORAGE = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('input'),
+};
+const formData = {
+  email: '',
+  message: '',
+};
+populateTextarea();
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+  localStorage.removeItem(STORAGE);
+  e.currentTarget.reset();
+  console.log(formData);
+});
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+  const stringifiedData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE, stringifiedData);
+}
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE));
+  if (savedMessage === null) {
+    return;
+  }
+
+  refs.textarea.value = savedMessage.message || '';
+  refs.input.value = savedMessage.email || '';
+  formData.email = savedMessage.email || '';
+  formData.message = savedMessage.message || '';
+}
